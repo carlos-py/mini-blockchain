44920e6f7ae600cc437ca9185fc32d0b062883b1
Carlos Vasquez <carlos@cobranix.com>
2017-07-23 14:33:39 -0700

    Consolidate files

--- minicoin-genesis.py
+++
@@ -1,7 +0,0 @@
-import date as date
-
-def create_genesis_block():
-    # Manually construct a block
-    # index zero and arbitrary previous hash
-
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
--- minicoin-new-block.py
+++
@@ -1,7 +0,0 @@
-def next_block(last_block):
-    this_index = last_block.index + 1
-    this_timestamp = date.datetime.now()
-    this_data = "This is block number: " + str(this_index)
-    this_hash = last_block.hash
-    return Block(this_index, this_timestamp, this_data, this_hash)
-
